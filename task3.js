e96a5a9c6ef7d4444dff7089f3b5673939ea5b20
xel26 <zhxo111nvshq@gmail.com>
2023-10-25 18:28:46 +0700

    task3 done with built-in-function. program without built-in-function on progress

--- task3.js
+++ task3.js
@@ -1,6 +1,27 @@
 const nilai = (nilaiAwal, nilaiAkhir, dataArray) => {
-    jumlahData = dataArray.length
-    
+    const jumlahData = dataArray.length
+    const output = []
+
+    if(nilaiAwal < nilaiAkhir){
+        if(jumlahData > 5){
+            for (const data of dataArray) {
+                if(data >= nilaiAwal && data <= nilaiAkhir){
+                    output.push(data)
+                }
+            }
+
+            if(output.length != 0){
+                console.log(`output: [${output}]`)
+            }else{
+                console.log('output: "Nilai tidak ditemukan"')
+            }
+            
+        }else{
+            console.log('output: "Jumlah angka dalam dataArray harus lebih  dari 5"')
+        }
+    }else{
+        console.log('output: "Nilai Akhir harus lebih besar dari nilai awal"')
+    }
 }
 
 nilai(5, 20, [2, 25, 4, 14, 17, 30, 8])
