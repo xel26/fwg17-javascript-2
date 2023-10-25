a7b4567eb020694eb1c3ac6bce96ad09c1a0d758
xel26 <zhxo111nvshq@gmail.com>
2023-10-25 19:38:00 +0700

    mengurutkan output dan menambahkan alur program. program dengan algoritma manual dan tanpa built-in-function on progress

--- task2.js
+++ task2.js
@@ -22,39 +22,39 @@ const names = [
 
 // built in function :
 const filter = (filter, limit) => {
-    let filterName = []
+    const filterName = []                                                                           // 1. deklarasi variable filterName = untuk menyimpan data nama yang mengandung huruf tertentu yang ada di parameter filter
 
-    for (const name of names) {
-        let lowerCaseName = name.toLowerCase()
-        if(lowerCaseName.includes(filter)){
-            const firstLetterLower = lowerCaseName.charAt(0)
-            const firstLetterUpper = firstLetterLower.toUpperCase()
-            const capitalize = lowerCaseName.replace(firstLetterLower, firstLetterUpper)
-            filterName.push(capitalize)
+    for (const name of names) {                                                                     // 2. melooping array names
+        let lowerCaseName = name.toLowerCase()                                                      // 3. tiap data dalam array names di jadikan huruf kecil
+        if(lowerCaseName.includes(filter)){                                                         // 4. pengkondisian = jika data nama mengandung huruf tertentu yang ada di parameter filter
+            const firstLetterLower = lowerCaseName.charAt(0)                                        // 5. mengambil huruf pertama dari nama
+            const firstLetterUpper = firstLetterLower.toUpperCase()                                 // 6. membuat huruf pertama dari nama menjadi huruf kapital
+            const capitalize = lowerCaseName.replace(firstLetterLower, firstLetterUpper)            // 7. mengganti huruf pertama kecil menjadi huruf pertama kapital
+            filterName.push(capitalize)                                                             // 8. memasukan nama yang mengandung huruf tertentu ke dalam array filterName
         }
     }
 
-    display(limit, filterName)
+    display(limit, filterName)                                                                      // 9 . memanggil function display dengan mengirim parameter limit dan parameter variable filtername
 
 }
 
 
 const display = (limit, filterName) => {
-    let filterNameLimit = []
+    const filterNameLimit = []                                                                      // 10. deklarasi variable filterNameLimit = untuk menyimpan data nama sesuai limit
 
-    for(let i = 0; i < limit; i++){
-        filterNameLimit.push(filterName[i])
+    for(let i = 0; i < limit; i++){                                                                 // 11. melooping data nama di filterName dan membatasi jumlahnya sebanyak limit
+        filterNameLimit.push(filterName[i])                                                         // 12. memasukan data nama ke dalam variable filterNameLimit
     }
 
-    console.log(filterNameLimit)
+    console.log(filterNameLimit)                                                                    // 13. menampilkan output
 }
 
 
 const searchName = (filter, limit, callback) => {
-    callback(filter, limit)
+    callback(filter, limit)                                                                         // 14. memanggil callback
 }
 
-searchName("ca", 3, filter)
+searchName("an", 3, filter)                                                                         // 15. memanggil function searchName dengan mengirim parameter filter, limit, dan callback function 
 
 
 
--- task3.js
+++ task3.js
@@ -1,26 +1,28 @@
 const nilai = (nilaiAwal, nilaiAkhir, dataArray) => {
-    const jumlahData = dataArray.length
-    const output = []
+    const jumlahData = dataArray.length                                                             // 1. mengambil jumlah data dalam dataArray
+    const output = []                                                                               // 2. deklarasi variable output = untuk tempat menyimpan data yang di cari
 
-    if(nilaiAwal < nilaiAkhir){
-        if(jumlahData > 5){
-            for (const data of dataArray) {
-                if(data >= nilaiAwal && data <= nilaiAkhir){
-                    output.push(data)
+    if(nilaiAwal < nilaiAkhir){                                                                     // 3. pengkondisian pertama true = nilai awal lebih kecil dari nilai akhir akhir
+        if(jumlahData > 5){                                                                         // 5. pengkondisian kedua true = jumlah data harus lebih dari 5
+            for (const data of dataArray) {                                                         // 7. melooping dataArray
+                if(data >= nilaiAwal && data <= nilaiAkhir){                                        // 8. pengkondisian = mencari data yang memiliki nilai diantara nilaiAwal dan nilaiAkhir
+                    output.push(data)                                                               // 9. memasukan nilai yang sesuai ke dalam variable output
                 }
             }
 
-            if(output.length != 0){
-                console.log(`output: [${output}]`)
+            output.sort((a, b) => a - b);                                                           // 10. mengurutkan output
+
+            if(output.length != 0){                                                                 // 11. pengkondisian = memeriksa variable output kosong atau tidak
+                console.log(output)                                                                 // 12. menampilkan nilai jika output tidak kosong
             }else{
-                console.log('output: "Nilai tidak ditemukan"')
+                console.log('output: "Nilai tidak ditemukan"')                                      // 13. menampilkan pesan jika output kosong
             }
-            
+
         }else{
-            console.log('output: "Jumlah angka dalam dataArray harus lebih  dari 5"')
+            console.log('output: "Jumlah angka dalam dataArray harus lebih  dari 5"')               // 6. pengkondisian kedua false = jumlah data kurang atau sama dengan 5
         }
     }else{
-        console.log('output: "Nilai Akhir harus lebih besar dari nilai awal"')
+        console.log('output: "Nilai Akhir harus lebih besar dari nilai awal"')                      // 4. pengkondisian pertama false = nilai awal lebih besar dari nilai akhir
     }
 }
 
@@ -31,3 +33,9 @@ nilai(15, 3, [2, 25, 4, 14, 17, 30, 8])
 nilai(4, 17, [2, 25, 4])
 
 nilai(5, 17, [2, 25, 4, 1, 30, 18])
+
+
+
+const data = [5, 7, 0, 8]
+data.sort()
+console.log(data)
